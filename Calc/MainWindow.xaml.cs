4001ee76410317f605e108869e5fd76365ad1b06
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-04-24 23:02:18 +0300

    Add files via upload
    Finished the calculator

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -1,63 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calc
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        //private ReversePolishNotation _reversPolishNotation = new ReversePolishNotation();
         ReversePolishNotation _reversPolishNotation = new ReversePolishNotation();
         private string _typedText = "";
         private int _hasOpeningBracket = 0;
+        private bool _hasOperationUsed;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_InputTextExample(object sender, RoutedEventArgs e)
+        private void Button_InputNumberExample(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-            _typedText += (string)button.Content + " ";
-            CalculationResultText.Text = _typedText + " ";
-            _reversPolishNotation.Input = _typedText + " ";
-            //ResultRPNText.Clear();
-            //ResultRPNText.Text = _reversPolishNotation.outp.ToString();
+            _typedText += (string)button.Content;
+            CalculationResultText.Text = _typedText;
+            _reversPolishNotation.Input = _typedText;
+            _hasOperationUsed = true;
+        }
 
-
+        private void Button_InputTheOperation(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (_hasOperationUsed || button.Content.ToString() == "-")
+            {
+                if (string.IsNullOrEmpty(_typedText) || (_typedText.EndsWith(" ") && button.Content.ToString() == "-"))
+                {
+                    _typedText += (string)button.Content;
+                    CalculationResultText.Text = _typedText;
+                    _hasOperationUsed = false;
+                }
+                else
+                {
+                    _typedText += " " + (string)button.Content + " ";
+                    CalculationResultText.Text = _typedText;
+                    _hasOperationUsed = false;
+                }
+            }
         }
 
         private void Button_BracketInput(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-
-            //if ((string)button.Content == "(")
             switch (button.Content)
             {
                 case "(":
-                    _typedText += (string)button.Content;
+                    _typedText += " " + (string)button.Content + " ";
                     CalculationResultText.Text = _typedText;
                     _hasOpeningBracket++;
                     break;
-
                 case ")":
                     if(_hasOpeningBracket != 0)
                     {
-                        _typedText += (string)button.Content;
+                        _typedText += " " + (string)button.Content + " ";
                         CalculationResultText.Text = _typedText;
                         _hasOpeningBracket--;
                     }
@@ -67,13 +67,25 @@ namespace Calc
 
         private void Button_Equals(object sender, RoutedEventArgs e)
         {
-            //ResultRPNText.Text = _reversPolishNotation.outp.ToString();
-
             _reversPolishNotation.LineInRPN();
-            
-            ResultRPNText.Text = _reversPolishNotation.outp.ToString();
-            CalculationResultText.Clear();
+            ResultRPNText.Text = _reversPolishNotation.output.ToString();
+            _reversPolishNotation.CalculateRPN(_reversPolishNotation.output);
+            CalculationResultText.Text = _reversPolishNotation.output.ToString();
+        }
 
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            _reversPolishNotation.output = "";
+            _typedText = null;
+            CalculationResultText.Clear();
+            ResultRPNText.Clear();
+        }
+
+        private void Button_InputDot(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            _typedText += (string)button.Content;
+            CalculationResultText.Text = _typedText;
         }
     }
 }
--- Calc/ReversePolishNotation.cs
+++ Calc/ReversePolishNotation.cs
@@ -10,20 +10,17 @@ namespace Calc
             get { return _input; }
             set { _input = value; }
         }
-        public string outp
+        public string output
         {
-            get { return output; }
-            set { output = value; }
+            get { return _output; }
+            set { _output = value; }
         }
         private string _input;
-        private string output = "";
-
+        private string _output = "";
 
         public void LineInRPN ()
         {
-            
             string[] tokens = _input.Split();
-
             Stack<string> stack = new Stack<string>();
 
 
@@ -34,7 +31,7 @@ namespace Calc
                 bool isNumber = double.TryParse(token, out number);
                 if (isNumber)
                 {
-                    output += token + " ";
+                    _output += token + " ";
                 }
 
                 // If token is an operator, pop previous operators from the stack and add them to the output
@@ -42,7 +39,7 @@ namespace Calc
                 {
                     while (stack.Count > 0 && IsOperator(stack.Peek()) && ComparePrecedence(token, stack.Peek()) <= 0)
                     {
-                        output += stack.Pop() + " ";
+                        _output += stack.Pop() + " ";
                     }
                     stack.Push(token);
                 }
@@ -58,7 +55,7 @@ namespace Calc
                 {
                     while (stack.Count > 0 && stack.Peek() != "(")
                     {
-                        output += stack.Pop() + " ";
+                        _output += stack.Pop() + " ";
                     }
                     stack.Pop();
                 }
@@ -67,14 +64,14 @@ namespace Calc
             // Pop any remaining operators from the stack and add them to the output
             while (stack.Count > 0)
             {
-                output += stack.Pop() + " ";
+                _output += stack.Pop() + " ";
             }
 
             //Console.WriteLine("RPN: " + output.Trim());
         }
 
         // Method to check if a token is an operator
-        static bool IsOperator(string token)
+        public bool IsOperator(string token)
         {
             return token == "+" || token == "-" || token == "*" || token == "/" || token == "^";
         }
@@ -90,12 +87,52 @@ namespace Calc
                 {"/", 2},
                 {"^", 3}
             };
-
             int op1Precedence = precedence[op1];
             int op2Precedence = precedence[op2];
-
             return op1Precedence.CompareTo(op2Precedence);
         }
 
+        public void CalculateRPN(string _output)
+        {
+            string[] tokens = _output.Split(' ');
+            Stack<double> stack = new Stack<double>();
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    stack.Push(number);
+                }
+                else if (token != "")
+                {
+                    double operand2 = stack.Pop();
+                    double operand1 = stack.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            stack.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            stack.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            stack.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            stack.Push(operand1 / operand2);
+                            break;
+                        case "^":
+                            stack.Push(Math.Pow(operand1, operand2));
+                            break;
+                        default:
+                            throw new ArgumentException("Invalid operator: " + token);
+                    }
+                }
+                //else return stack.Pop();
+            }
+            //return stack.Pop();
+            output = stack.Pop().ToString();
+        }
     }
 }
